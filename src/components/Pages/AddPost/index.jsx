7e8f4b385e2a632c2b97194ce9ae2aff43dea358
jasminaisaeva <jasminaisaeva2001@gmail.com>
2021-11-06 18:14:54 +0600

    added icons with person page,added page Message,Likes,Profile,AddPost.ON page AddPost added Modal window

---
+++ src/components/Pages/AddPost/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BasicModal } from "./BasicModal";
+
+export const AddPost = () => {
+ 
+  return (
+    <div>
+      <BasicModal />
+    </div>
+  );
+};
---
+++ src/components/Pages/Likes/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Likes = () => {
+    return <div>
+        Likes
+    </div>
+}
