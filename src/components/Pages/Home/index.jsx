a268589eb8cb590020ebf1fa4b9d1a614283ae3b
jasminaisaeva <jasminaisaeva2001@gmail.com>
2021-11-07 22:47:25 +0600

    done registration and logOut pages

--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 
 export const Home = () => {
   const users = useSelector((state) => state.user);
-  // const userNAme = users.displayName;
   console.log(users);
   return <div>{users.name}</div>;
 };
