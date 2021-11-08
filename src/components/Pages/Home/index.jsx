abbe3596367ea57de444599a3a1040e9f84e180d
unknown <nurperiarss@gmail.com>
2021-11-08 12:56:59 +0600

    test

--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 
 export const Home = () => {
   const users = useSelector((state) => state.user);
-  const userNAme = users?.claims?.name;
+ 
   console.log(userNAme);
   return <div>{userNAme}</div>;
 };
